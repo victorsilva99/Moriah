b9994c339b96f0db432c1a1833c986486745eeae
Victor Silva <fe605@t-systems.com.br>
2023-04-17 22:00:47 -0300

    Initial Commit

---
+++ src/Moriah.Domain/Entities/Caixa.cs
@@ -0,0 +1,11 @@
+﻿namespace Moriah.Domain.Entities;
+
+public class Caixa
+{
+    public Guid Id { get; set; }
+    public DateTime Data { get; set; }
+    public double? Nota { get; set; }
+    public double? Moeda { get; set; }
+    public double? Cartao_Debito { get; set; }
+    public double? Cartao_Credito { get; set; }
+}
---
+++ src/Moriah.Infra.Data/Context/MoriahDataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Moriah.Domain.Entities;
+
+namespace Moriah.Infra.Data.Context;
+
+public class MoriahDataContext : DbContext
+{
+    public DbSet<Caixa> Entradas { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        options.UseSqlServer("");
+    }
+}
