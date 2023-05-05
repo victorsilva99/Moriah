26fbdea9139c5c023b716de50d0fbe98c46cd3a6
Victor Silva <fe605@t-systems.com.br>
2023-05-05 02:01:35 -0300

    feat: adicionado notfy

--- src/Moriah.Web/wwwroot/js/site.js
+++ src/Moriah.Web/wwwroot/js/site.js
@@ -1,4 +1,29 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿const notyf = new Notyf({
+    duration: 3000,
+    position: {
+        x: 'right',
+        y: 'top',
+    },
+    types: [
+        {
+            type: 'warning',
+            background: 'orange',
+            icon: {
+                className: 'material-icons',
+                tagName: 'i',
+                text: 'warning'
+            }
+        },
+        {
+            type: 'error',
+            background: 'indianred',
+            duration: 2000,
+            dismissible: true
+        },
+        {
+            type: 'info',
+            background: 'blue',
+            icon: false
+        }
+    ]
+});
---
+++ src/Moriah.Web/wwwroot/js/views/caixa/index.js
@@ -0,0 +1,7 @@
+notyf.open({
+    type: 'warning',
+    message: 'Send us <b>an email</b> to get support',
+    icon: false
+});
+
+notyf.success('test');
